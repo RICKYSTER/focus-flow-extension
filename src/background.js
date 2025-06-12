ded80d066fac70b2aa6c82798161a356631b5859
Sobhan <63411627+RICKYSTER@users.noreply.github.com>
2025-06-12 12:00:39 +0530

    Update background.js

--- src/background.js
+++ src/background.js
@@ -2,6 +2,7 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("FocusFlow Extension Installed.");
 });
+
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.action === "closeTabAfter" && sender.tab && msg.minutes) {
     const delayMs = msg.minutes * 60 * 1000;
@@ -9,5 +10,12 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     setTimeout(() => {
       chrome.tabs.remove(sender.tab.id);
     }, delayMs);
+  }
+
+  // Immediate force close after countdown
+  if (msg.action === "forceCloseTab") {
+    if (sender.tab && sender.tab.id) {
+      chrome.tabs.remove(sender.tab.id);
+    }
   }
 });
